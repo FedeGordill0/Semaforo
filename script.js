f6bc7a9569809b7d7f4a28aab90263f9c674a93f
Federico Gordillo <fede1234gordillo@gmail.com>
2023-04-03 12:11:13 -0300

    Código

---
+++ script.js
@@ -0,0 +1,34 @@
+let estadoSemaforo = 0;
+
+const cambiarEstado = () => {
+  switch (estadoSemaforo) {
+    case 0:
+      cambiarColor("red", true);
+      cambiarColor("yellow", false);
+      cambiarColor("green", false);
+      break;
+    case 1:
+      cambiarColor("red", false);
+      cambiarColor("yellow", false);
+      cambiarColor("green", true);
+      break;
+    case 2:
+      cambiarColor("red", false);
+      cambiarColor("yellow", true);
+      cambiarColor("green", false);
+      break;
+  }
+  estadoSemaforo++;
+
+  if (estadoSemaforo > 2) {
+    estadoSemaforo = 0;
+  }
+};
+
+const cambiarColor = (id, prender) => {
+  const elemento = document.getElementById(id);
+  elemento.style.backgroundColor = prender ? id : "gray";
+};
+
+const btn = document.getElementById("btn");
+btn.addEventListener("click", cambiarEstado);
